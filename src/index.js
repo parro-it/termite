133c0dc73508935f8881b6c5a9dc6cfb74a795dc
Andrea Parodi <andrea@parro.it>
2015-09-18 21:29:46 +0200

    moved to src folder

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ app.on('ready', () => {
   Menu.setApplicationMenu(menu);
 
 
-  mainWindow.showUrl('index.html', () => {
+  mainWindow.showUrl(__dirname + '/index.html', () => {
     mainWindow.maximize();
   });
 });
