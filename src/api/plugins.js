8ff7a3a19de768f288f1083791e5e3e50b29a679
Andrea Parodi <andrea@parro.it>
2015-10-23 21:19:05 +0200

    plugin install WIP

--- src/api/plugins.js
+++ src/api/plugins.js
@@ -2,11 +2,13 @@
 
 const EventEmitter = require('events').EventEmitter;
 const PluginLoader = require('plugin-loader').PluginLoader;
-
+const npmKeyword = require('npm-keyword');
+const npm = require('enpeem');
+const co = require('co');
 
 module.exports = Object.assign(new EventEmitter(), {
   load() {
-    const loader = new PluginLoader([this.app.config.configFolder + '/plugins/node_modules']);
+    const loader = new PluginLoader([this.pluginsFolder + '/node_modules']);
     loader.on('pluginLoaded', (pluginName, plugin) => {
       const pluginResult = plugin(this.app);
       this.app.packages[pluginResult.name] = pluginResult;
@@ -26,5 +28,23 @@ module.exports = Object.assign(new EventEmitter(), {
 
   init(app) {
     this.app = app;
+    this.pluginsFolder = app.config.configFolder + '/plugins';
+
+    app.commands.register('install-package', co.wrap(function * () {
+      const plugins = yield npmKeyword.names('termite-plugin');
+      const pluginToInstall = yield app.palette.open(plugins);
+      const opts = {
+        dir: app.config.configFolder + '/plugins',
+        dependencies: [pluginToInstall],
+        loglevel: 'verbose'
+      };
+
+      npm.install(opts, err => {
+        if (err) {
+          return process.stderr.write('Error occurred while installing package: ' + err.stack + '\n');
+        }
+        alert(pluginToInstall + ' installed.');
+      });
+    }));
   }
 });
