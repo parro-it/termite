ef22485c97aa237658e0e83304f0d9b6ea0b3339
Andrea Parodi <andrea@parro.it>
2015-10-23 20:02:17 +0200

    palette api

---
+++ src/api/palette.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const EventEmitter = require('events').EventEmitter;
+
+
+module.exports = Object.assign(new EventEmitter(), {
+  open(items) {
+    const palette = require('command-palette');
+    const p = palette.create(items);
+    return new Promise(resolve => {
+      p.on('cancel', resolve);
+      p.on('command', resolve);
+      p.appendTo(document.body);
+      p.show();
+    });
+  },
+
+  init() {}
+});
